fc62fe730bbb11810052fca899cf5be4c4200d96
Abergaz <zagreba@gmail.com>
2019-03-21 13:26:40 +0300

    1927 v. 2019.03.21 13-00 правильный вариант с \\r\\n String[] arr = s.split("\\r\\n"); но его не принимает валидатор, а принимает только с \\n

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+В методе main подмени объект System.out написанной тобой реадер-оберткой.
+Твоя реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строки: "first","second","third","fourth","fifth".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c конструктором принимающим ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строки(вставлять контекстную рекламу) выведенные методом printSomething() согласно заданию, и выводить её в консоль.
+*/
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        printStream.close();
+        System.setOut(console);
+        String s = byteArrayOutputStream.toString();
+        String[] arr = s.split("\\r\\n");
+        for(int i=0; i<arr.length; i++){
+            if ((i+1)%2==0){
+                System.out.println(arr[i]);
+                System.out.println("JavaRush - курсы Java онлайн");
+            }else{
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
