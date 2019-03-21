a9b856dcf5f372976f06e8eca7c19077f932b4b2
Abergaz <zagreba@gmail.com>
2019-03-21 11:05:35 +0300

    1925 v. 2019.03.21 11-00 Варианс с regExp только буквы в словах

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task19.task1925;
+
+/* 
+Длинные слова
+В метод main первым параметром приходит имя файла1, вторым - файла2.
+Файл1 содержит слова, разделенные пробелом.
+Записать через запятую в Файл2 слова, длина которых строго больше 6.
+В конце файла2 запятой не должно быть.
+Закрыть потоки.
+
+Пример выходных данных в файл2:
+длинное,короткое,аббревиатура
+
+
+Требования:
+1. Программа НЕ должна считывать данные с консоли.
+2. Программа должна считывать содержимое первого файла (используй FileReader c конструктором String).
+3. Поток чтения из файла (FileReader) должен быть закрыт.
+4. Программа должна записывать через запятую во второй файл все слова из первого файла длина которых строго больше 6(используй FileWriter).
+5. Поток записи в файл (FileWriter) должен быть закрыт.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader(args[0]);
+        FileWriter fileWriter = new FileWriter(args[1]);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        boolean isFirst=true;
+        while (bufferedReader.ready()){
+            String[] arr = bufferedReader.readLine().trim().split("\\s");
+            for (int i=0;i<arr.length; i++){
+                if (arr[i].matches("[a-яё]{7,}")){
+                    if (isFirst) {
+                        bufferedWriter.write(arr[i]);
+                        isFirst=false;
+                    }else{
+                        bufferedWriter.write(",");
+                        bufferedWriter.write(arr[i]);
+                    }
+                }
+            }
+        }
+        bufferedReader.close();
+        fileReader.close();
+        bufferedWriter.close();
+        fileWriter.close();
+    }
+}
