c32458704cc0f3735ada79de514998e8ef60ca13
Abergaz <zagreba@gmail.com>
2019-03-22 11:09:51 +0300

    1918 v. 2019.03.22 11-00 Вариант с использованием библиотеки JSOUP

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.task.task19.task1918;
+
+/* 
+Знакомство с тегами
+Считайте с консоли имя файла, который имеет HTML-формат.
+
+Пример:
+Info about Leela <span xml:lang="en" lang="en"><b><span>Turanga Leela
+</span></b></span><span>Super</span><span>girl</span>
+
+Первым параметром в метод main приходит тег. Например, "span".
+Вывести на консоль все теги, которые соответствуют заданному тегу.
+Каждый тег на новой строке, порядок должен соответствовать порядку следования в файле.
+Количество пробелов, \n, \r не влияют на результат.
+Файл не содержит тег CDATA, для всех открывающих тегов имеется отдельный закрывающий тег, одиночных тегов нет.
+Тег может содержать вложенные теги.
+
+Пример вывода:
+<span xml:lang="en" lang="en"><b><span>Turanga Leela</span></b></span>
+<span>Turanga Leela</span>
+<span>Super</span>
+<span>girl</span>
+
+Шаблон тега:
+<tag>text1</tag>
+<tag text2>text1</tag>
+<tag
+text2>text1</tag>
+
+text1, text2 могут быть пустыми
+
+
+Требования:
+1. Программа должна считывать имя файла с консоли (используй BufferedReader).
+2. BufferedReader для считывания данных с консоли должен быть закрыт.
+3. Программа должна считывать содержимое файла (используй FileReader).
+4. Поток чтения из файла (FileReader) должен быть закрыт.
+5. Программа должна выводить в консоль все теги, которые соответствуют тегу, заданному в параметре метода main.
+*/
+
+import java.io.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName=bufferedReader.readLine();
+        bufferedReader.close();
+        File file = new File(fileName);
+
+        Document doc = Jsoup.parse(file,"UTF-8");
+        String s = doc.getElementsByTag(args[0]).outerHtml();
+        System.out.println(s);
+
+
+    }
+
+}
